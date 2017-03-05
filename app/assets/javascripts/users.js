449c8109747fc0c9af21a97fbeee79f06878f2b0
Soumya Kedlaya <soumyakedlaya@Soumyas-MacBook-Air.local>
2017-03-04 23:01:13 -0800

    added picture, changed js file

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -10,26 +10,51 @@ $(document).ready(function(){
   $('#donor_signup').click(function(){
     if(gRegFormOpen == true){
       $('#charityRep').hide();
-    }
-    if('#donor_reg_errors'.length >0 || '#charityRep_reg_errors'.length >0){
-      $('#donor_reg_errors').hide();
       $('#charityRep_reg_errors').hide();
     }
+    // if($('#donor_reg_errors').length >0 || '#charityRep_reg_errors'.length >0){
+    //   $('#donor_reg_errors').hide();
+    //   $('#charityRep_reg_errors').hide();
+    // }
 
     $('#donor').toggle(effect, options, duration);
     gRegFormOpen = true;
   });
 
-  //when user clicks sign up your charity, check if a registration form is open. If yes --> hide form. Check if errors box is open. If yes --> hide. Toggle slide charityRep registration form and set gRegFormOpen to true
+  if($('#donor_reg_errors').length >0){
+    $('#donor_reg_errors').show();
+    $('#donor').show();
+    gRegFormOpen = true;
+  }
+
+
+
+  // $('#reg_as_donor').click(function(){
+  //   console.log("In reg_as_donor click function")
+  //   if ('#donor_reg_errors'.length > 0){
+  //     console.log("Length of donor_reg_errors: " + '#donor_reg_errors.length')
+  //     $('#reg_as_donor').show();
+  //     $('#donor_reg_errors').show();
+  //   }
+  // });
+
+  // when user clicks sign up your charity, check if a registration form is open. If yes --> hide form. Check if errors box is open. If yes --> hide. Toggle slide charityRep registration form and set gRegFormOpen to true
   $('#charityRep_signup').click(function(){
     if (gRegFormOpen == true){
       $('#donor').hide();
-    }
-    if('#charityRep_reg_errors'.length >0 || '#donor_reg_errors'.length >0){
-      $('#charityRep_reg_errors').hide();
       $('#donor_reg_errors').hide();
     }
+    // if('#charityRep_reg_errors'.length >0 || '#donor_reg_errors'.length >0){
+    //   $('#charityRep_reg_errors').hide();
+    //   $('#donor_reg_errors').hide();
+    // }
     $('#charityRep').toggle(effect, options, duration);
     gRegFormOpen = true;
   });
+
+  if($('#charityRep_reg_errors').length >0){
+    $('#charityRep_reg_errors').show();
+    $('#charityRep').show();
+    gRegFormOpen = true;
+  }
 });
